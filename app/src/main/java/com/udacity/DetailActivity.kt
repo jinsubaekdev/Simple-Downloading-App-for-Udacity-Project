f2a8714f5502029b062cc34310d1add065a94b9c
Jinsu Baek <jinsu350@gmail.com>
2021-06-21 10:42:03 +0900

    Update from making new task to finishing activity

--- app/src/main/java/com/udacity/DetailActivity.kt
+++ app/src/main/java/com/udacity/DetailActivity.kt
@@ -1,18 +1,12 @@
 package com.udacity
 
-import android.content.Intent
+import android.app.NotificationManager
 import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.motion.widget.MotionLayout
-import androidx.databinding.DataBindingUtil
-import com.udacity.databinding.ActivityDetailBinding
-import com.udacity.databinding.ContentDetailBinding
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_detail.*
 import kotlinx.android.synthetic.main.content_detail.*
 
@@ -26,6 +20,9 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
         setSupportActionBar(toolbar)
+
+        val notificationManager = ContextCompat.getSystemService(this, NotificationManager::class.java) as NotificationManager
+        notificationManager.cancelAll()
 
         val downloadTitle = intent.getStringExtra(MainActivity.DOWNLOAD_TITLE)
         val downloadStatus = intent.getStringExtra(MainActivity.DOWNLOAD_STATUS)
@@ -42,16 +39,13 @@ class DetailActivity : AppCompatActivity() {
 
             override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {}
 
-            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
-                val intent = Intent(applicationContext, MainActivity::class.java)
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-                startActivity(intent)
+            override fun onTransitionCompleted(p0: MotionLayout?, currentId: Int) {
+                if(currentId == R.id.end)
+                    finish()
             }
 
             override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {}
         }
         motion_layout.addTransitionListener(motionListener)
     }
-
 }
